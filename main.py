05cb2a6f3863feb721ccfa437302f3311fa76d55
Your Name <siddhrathyadav1999@gmail.com>
2019-04-15 15:18:19 +0530

    init change

---
+++ main.py
@@ -0,0 +1,71 @@
+
+import pandas as pd
+#%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("All Libraries Imported")
+
+
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,encoding='latin-1')
+
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,encoding='latin-1')
+
+
+i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
+encoding='latin-1')
+
+print(users.shape)
+users.head()
+
+print(ratings.shape)
+ratings.head()
+
+print(items.shape)
+items.head()
+
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+ratings_train.shape, ratings_test.shape
+
+
+n_users = ratings.user_id.unique().shape[0]
+n_items = ratings.movie_id.unique().shape[0]
+
+
+data_matrix = np.zeros((n_users, n_items))
+for line in ratings.itertuples():
+    data_matrix[line[1]-1, line[2]-1] = line[3]
+
+
+from sklearn.metrics.pairwise import pairwise_distances 
+user_similarity = pairwise_distances(data_matrix, metric='cosine')
+item_similarity = pairwise_distances(data_matrix.T, metric='cosine')
+
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        #We use np.newaxis so that mean_user_rating has same format as ratings
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+user_prediction = predict(data_matrix, user_similarity, type='user')
+item_prediction = predict(data_matrix, item_similarity, type='item')
+print(data_matrix)
+    
+print(user_similarity)
+
+print(user_prediction)
+print(item_prediction)
